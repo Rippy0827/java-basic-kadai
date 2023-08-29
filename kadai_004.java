1e9a8a01fcf456b64b7f4f929b2e8e6bdf168cd3
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-08-29 16:37:13 +0900

    Update and rename kadai_004 to kadai_004.java
    その他、11～21の列でplintlnのスペルミスを修正

---
+++ kadai_004.java
@@ -0,0 +1,27 @@
+public calss kadai_004{
+  public static void main(String[] args) {
+
+  int A , B, C , D , E , F , G , H , I , J;
+  int Heikin;
+
+    A = 10 , B= 20 , C = 30 , D = 40 , E = 50 
+    F = 60 , G = 70 , H = 80 , I = 90 , J = 100
+    Heikin = (A+B+C+D+E+F+G+H+I+J)/10
+
+      System.out.Plintln("Aさんの点数は" + A + "点");
+      System.out.Plintln("Bさんの点数は" + B + "点");
+      System.out.Plintln("Cさんの点数は" + C + "点");
+      System.out.Plintln("Dさんの点数は" + D + "点");
+      System.out.Plintln("Eさんの点数は" + E + "点");
+      System.out.Plintln("Fさんの点数は" + F + "点");
+      System.out.Plintln("Gさんの点数は" + G + "点");
+      System.out.Plintln("Hさんの点数は" + H + "点");
+      System.out.Plintln("Iさんの点数は" + I + "点");
+      System.out.Plintln("Jさんの点数は" + J + "点");
+      System.out.Plintln("テストの平均点は" + Heikin + "点");
+
+
+
+}
+
+}
