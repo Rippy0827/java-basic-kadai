f9797f9f1ae5067fa0056d489f2c81c2cbd84eea
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-08-29 16:35:15 +0900

    Data_1.javaを作成
    javaの基礎、第四章の教材

---
+++ Data_1.java
@@ -0,0 +1,9 @@
+palage java-basic-kadai;
+
+public class Data {
+  public static void main(String[] args) {
+    system.out.println(100);
+    System.out.println(0);
+    system.out.println(-123);
+  }
+}
