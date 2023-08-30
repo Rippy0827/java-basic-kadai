2f51f3b298b2ef62769615c8cdc2bc507e49b27a
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-08-30 11:04:19 +0900

    Create Data_7.java

---
+++ Data_7.java
@@ -0,0 +1,11 @@
+package java-basic-kadai{
+
+  public class Cata_7{
+    public static viod main(String[] args){
+      System.out.println(45 + 1.8);
+      System.out.println(3 - 1.2);
+      System.out.println(15 + 0.6);
+      System.out.println(3 + 0.3);
+      System.out.println(45 % 0.8);
+    }
+}
