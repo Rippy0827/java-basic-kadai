24180460433745f8a0a511c01fdede2f818582d4
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-02 17:09:42 +0900

    Delete Data_9.java

--- Data_9.java
+++
@@ -1,9 +0,0 @@
-package java-besic-kadai;
-
-public class Data_9 {
-  public static void main(String[] args){
-    System.out.println("侍テラコヤ" + 123 + 654); //パターン１
-    System.out.Println(123 + 654 + "侍テラコヤ"); //パターン２
-    system.out.println("侍テラコヤ" +(123 + 654));  //パターン３
-  }
-}
