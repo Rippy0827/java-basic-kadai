89097a640a2d1f800280bc68a96aed8248550c2b
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-02 17:01:49 +0900

    Delete Data_1.java

--- Data_1.java
+++
@@ -1,9 +0,0 @@
-palage java-basic-kadai;
-
-public class Data {
-  public static void main(String[] args) {
-    system.out.println(100);
-    System.out.println(0);
-    system.out.println(-123);
-  }
-}
