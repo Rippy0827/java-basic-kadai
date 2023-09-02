fb2d86a3a7523d03968a27cb20b85d74ebaa1114
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-02 17:03:00 +0900

    Create Data_3.java

---
+++ java_4th/Data_3.java
@@ -0,0 +1,9 @@
+packege java-basic-kadai{
+  public class Data_3{
+  
+  public static void main(String[] args){
+    system.out.println(0.1);
+    system.out.println(1.618);
+    system.out.println(-9876.0123);
+}
+}
