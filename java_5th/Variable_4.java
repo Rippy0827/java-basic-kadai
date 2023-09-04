5344ea8ce52481f5ba58b5114106264c7d7d0e33
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 11:52:31 +0900

    Create Variable_4.java

---
+++ java_5th/Variable_4.java
@@ -0,0 +1,14 @@
+package java-basic-kadai;
+  public static void main(String[] args){
+
+    //整数と浮動小数点の足し算
+    int    number1  =5;
+    float  number2  =2.5F;
+    System.ou.println(number1 + number2)
+
+    //文字列と文字列型の連結
+    String lastNmae  =  "侍";
+    String firstName =  "太郎";
+    System.out.println(lastName + firstName);
+  }
+}
