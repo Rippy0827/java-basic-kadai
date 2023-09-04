7119754b1b7aa5ee4e998d896c24efa351604574
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 11:45:33 +0900

    Create Variable_3.java

---
+++ java_5th/Variable_3.java
@@ -0,0 +1,14 @@
+package java-basic-kadai;
+
+public class Variable_3.java{
+  public static void main(String[] args){
+
+  //変数testに123をセット
+   int test = 123 ;
+    System.out.println(test);
+
+  //変数testの中身を456に変更
+  test = 456;
+    System.out.println(test);
+  }
+}
