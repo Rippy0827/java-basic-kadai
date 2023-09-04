363f3d276030bdb02e62fabea2906521eb509985
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 11:59:04 +0900

    Update variable_4.java

--- java_5th/variable_4.java
+++ java_5th/variable_4.java
@@ -4,17 +4,17 @@ package java-basic-kadai;
     //整数と浮動小数点の足し算
     int    number1  =5;
     float  number2  =2.5F;
-    System.ou.println(number1 + number2)
+    System.ou.println("number1 + number2 =" number1 + number2)
 
     //文字列と文字列型の連結
     String lastNmae  =  "侍";
     String firstName =  "太郎";
-    System.out.println(lastName + firstName);
+    System.out.println("lastName + firstName =" lastName + firstName);
 
     //補足
     int number3  = 32 ;
-    int number4  = number3 ? 4;
-    System.out.println(number4);
+    int number4  = number3 / 4;
+    System.out.println(number3 + "と" + number4);
 
   }
 }
