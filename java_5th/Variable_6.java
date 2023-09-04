844a9ced904fe610f0245d7a41c54c6ef514d9c8
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 13:51:18 +0900

    Create Variable_6.java

---
+++ java_5th/Variable_6.java
@@ -0,0 +1,19 @@
+package java-basic-kadai;
+
+public calss Variable_6;{
+    publicstatic viod main (String[] args){
+
+      final double   PI = 3.14159
+      double circArea   = 0, cirumference = 0;
+      int        radius = 5
+
+      //円の面積を求める
+      circleArea    = Pi * radius * radius;
+      System.out.println(circleArea);
+
+      //演習を求める
+      circumference = PI * (radius * 2);
+      System.out.println(circumference);
+    }
+}
+  
