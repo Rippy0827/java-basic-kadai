274364ffad6b3b236ca56e974af4b74a7c3bf9b0
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 11:40:56 +0900

    Create vaible_2.java
    第五章　変数を理解しよう。より

---
+++ java_5th/vaible_2.java
@@ -0,0 +1,33 @@
+package java-basic-kadai;
+
+public class variable_2{
+
+  //整数型の変数の宣言
+byte   testByte    = 1;
+short  testShort   = 12;
+int    testInt     = 123;
+long  　testLong   = 1234;
+
+// 浮動小数点型の変数を宣言
+float  testFloat   = 0.123F;  //代入する値の末尾にはFが必要
+double testDouble  = 0.123456F;
+
+// 倫理型の変数を宣言
+boolean testBoolean =ture;
+
+// 文字列型・文字型の変数を宣言
+String  testString  = "samurai";
+cahr    testChar    = '侍';　　//漢字も一文字として扱う
+
+//変数testの値を表示
+System.out.println(testByte);
+System.out.println(testShort);
+System.out.println(testInt);
+System.out.println(testLong);
+System.out.println(testFlort);
+System.out.println(testDouble);
+System.out.println(testBoolean);
+System.out.println(testString);
+System.out.println(testChar);
+}
+}
