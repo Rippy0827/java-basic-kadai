266570815c5b23f2a42936ff13e3b3601ca3d1ed
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 13:52:51 +0900

    Create variable_5.java

---
+++ java_5th/variable_5.java
@@ -0,0 +1,11 @@
+package java-basic-kadai;
+
+public class variable_5 {
+    public static void main(String[] args) {
+		
+        int number3 = 32;
+        int number4 = number3 / 4;
+        System.out.println("number3は" + number3);
+        System.out.println("number4は" + number4);     
+    }
+}
