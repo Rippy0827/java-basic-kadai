03f9344a51723f9a1c04b2e8779757403806ab40
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-04 18:12:47 +0900

    Create TypeConv_3.java

---
+++ java_6th/TypeConv_3.java
@@ -0,0 +1,15 @@
+package java-basic-kadai;
+
+//-------------------------キャスト演算子の有効性------------------------
+
+public class TpyConv_3{
+  public static void main(string[] args){
+
+    int distance = 120;
+    int time = 25;
+    //距離と所要時間から速度(m/秒)を計算
+
+  float speed = (float)distance / (float)time;
+  System.out.println("速度:" + speed + "m/秒");
+  }
+}
