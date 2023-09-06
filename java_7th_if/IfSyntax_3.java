ff336970222db5e059c6bbfe5c4f83e8c2c444ef
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-06 13:12:55 +0900

    Create IfSyntax_3.java

---
+++ java_7th_if/IfSyntax_3.java
@@ -0,0 +1,20 @@
+package text.section_07;
+
+public class IfSyntax_3 {
+  public static void main(String[] args)
+
+  int randdNum = (int(Math.random() * 10);
+
+  //乱数randNumの値を出力
+  System.out.println(ranNum);
+
+  //あたりはずれの判定
+  If(randNum == 9) {
+    System.out.println("大当たり");
+  } else if (runfNum == 8 ){
+    System.out.println("あたり");
+  } else {
+    System.out.println("大外れ―")
+      }
+}
+}
