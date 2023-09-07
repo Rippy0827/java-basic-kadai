f5f3d98b3730508eb090189e39656f092293df3a
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-07 11:58:42 +0900

    Create ForSyntax_2.java

---
+++ java_9th_for/ForSyntax_2.java
@@ -0,0 +1,16 @@
+package text.setion_09;
+
+public class ForSyntax_2{
+  public static void main (String[] args) { 
+
+    //　変数i=１～10の範囲で繰り返す
+    for( int i = 1; i < 11; i++) {
+        System.out.println(i);
+        //ループカウンターが5の時に繰り返し処理を強制終了
+        if ( i == 5 )
+        system.out.print("繰り返し処理を強制終了します")
+        break;
+    }
+  }
+}
+}
