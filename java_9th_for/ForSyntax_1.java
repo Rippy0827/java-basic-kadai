5210e0a85a5da3a2897fcbe7b90bfa82119aa5aa
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-07 11:12:46 +0900

    Create ForSyntax_1.java

---
+++ java_9th_for/ForSyntax_1.java
@@ -0,0 +1,11 @@
+package java_8th.java{
+
+public class ForSyntax_1{
+  public static void msin (string[] args);
+
+    ///変数i=１～10の範囲で繰り返す
+    for( int i = 1; i < 11; i++) {
+      System.out.println(i);
+    }
+  }
+}
