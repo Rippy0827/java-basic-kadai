b489fb718ea73e22629bd37328e6fa41b75b46c3
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-09 09:15:11 +0900

    Update kadai_011.java

--- kadai_011.java
+++ kadai_011.java
@@ -1,5 +1,28 @@
-package kadai_011;
+package text.section_11.java;
 
-public class Prime_Chapter11.java{
-  public static void main (String[] args){
-    int 
+	public class Arry_2 {
+	    public static void main(String[] args) {
+
+		    int[] SosuuArray = {2}; //素数配列
+    	    int i = 0;             //i:判定される数字
+
+    
+    	       for( int num = 3; num <= 101 ; num++ )   {  //num;2から始まる数字を100まで続ける
+
+    	          if (num / SosuuArray[i] != 0)            //--------割り切れない場合
+    	             System.out.println(num);				//num(2)を出力
+    	         
+
+    	              	          
+    	          
+    	          i ++;										//SosuuArry(i(0))に要素を１つ増やす
+    	          
+    	         SosuuArray[i] = num  ; //SosuuArry(0)にnum(2)をいれる。
+    	         
+    	         
+    	         continue;  
+
+    	       }
+	    }
+
+}
