c20c65d84c83d7c40d0b1a3ecc61fad66ce03d18
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-12 06:59:19 +0900

    Update kadai_011.java

--- kadai_011.java
+++ kadai_011.java
@@ -1,4 +1,4 @@
-package text.section_11.java;
+package text.section_011;
 
 public class Arry_2 {
 	public static void main(String[] args) {
@@ -11,38 +11,40 @@ public class Arry_2 {
 				if (num == 2) {//-------2の場合
 					SosuuArray[i] = num;//--素数2をSosuuArrayに収容
 					i++;
+					continue;
 				}
 
 			if (num % 3 == 0) //--------3割り切れる場合
 				if (num == 3) {
 					SosuuArray[i] = num;
 					i++;
+					continue;
 				}
 
 			if (num % 5 == 0) //--------5割り切れる場合
 				if (num == 5) {
 					SosuuArray[i] = num;
 					i++;
-
+					continue;
 				}
 
 			if (num % 7 == 0) //--------7割り切れる場合
 				if (num == 7) {
 					SosuuArray[i] = num;
 					i++;
-
+					continue;
 				}
 
-				else {
-
-					for (int a : SosuuArray) {
-						System.out.println(a);
-
-						continue;
-					}
+			if (num != 1)
+				if (num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num % 7 != 0) {
+					SosuuArray[i] = num;
+					i++;
+					continue;
 				}
-
+					/*			for (int outNum : SosuuArray)
+									//出力する
+									System.out.println(outNum);*/
 		}
-
-	}
+	}	
 }
+
