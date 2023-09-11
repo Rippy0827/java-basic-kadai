a63f38769223c2e36b07360c0ba9bc841622da66
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-11 18:15:03 +0900

    Create Method_1.java

---
+++ java_012_method/Method_1.java
@@ -0,0 +1,13 @@
+package text.section_12;
+
+pulic class Method_1{
+    public static void main(String[] args){
+
+    }
+    //朝のあいさつメッセージを表示するメソッド
+  public static void greeting() {
+    System.out.println("おはようございます");
+    System.out.println("きのうはよく眠れましたか？");
+    System.out.println("今日も一日がんばりましょう！");
+  }
+}
