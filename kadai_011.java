1bcaf306cb3637f2eb92bf3049ce408eb6b12900
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-11 09:15:02 +0900

    Update kadai_011.java

--- kadai_011.java
+++ kadai_011.java
@@ -3,18 +3,41 @@ package text.section_11.java;
 	public class Arry_2 {
 	    public static void main(String[] args) {
 
-	    int[] SosuuArray = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97}; //素数配列
+	    int[] SosuuArray = {25}; //素数配列
     	    int i = 0;             //i:判定される数字
 
     
-    	       for( int num = 3; num <= 101 ; num++ )   {  //num;2から始まる数字を100まで続ける、次からnum+1
+    	       for( int num = 1; num <= 101 ; num++ )   {  //num;2から始まる数字を100まで続ける、次からnum+1
+    	       if (num % 2 == 0)			//--------2割り切れない場合
+		 if (num == 2) {
+		 SosuuArry[i]=num;
+		　i++:}
 
-    	          if (num / SosuuArray[i] != 0)            //--------割り切れない場合
-    	             				
-    	          i ++;										//SosuuArry(i(0))に要素を１つ増やす
-    	          
-    	         SosuuArray[i] = num  ; //SosuuArry(0)にnum(2)をいれる。
-    	         System.out.println(num);	//num(2)を出力
+	       if (num % 3 == 0)			//--------3割り切れない場合
+		 if (num == 3) {
+		 SosuuArry[i]=num;
+		 i++;}
+			 
+	       if (num % 5 == 0)			//--------5割り切れない場合
+		 if (num == 5) {
+		 SosuuArry[i]=num;
+		 i++; }
+			 
+	       if (num % 7 == 0)			//--------7割り切れない場合
+		 if (num == 7) {
+		 SosuuArry[i]=num;			 
+    	          i ++; }
+
+	       if (num % 13 == 0)			//--------7割り切れない場合
+		 if (num == 13) {
+		 SosuuArry[i]=num;			 
+    	          i ++; }
+			 
+		else {
+		SosuuArry[i] = num;			//SosuuArry(i(0))に要素を１つ増やす
+			 
+		for (int a : Sosuu Array){ 
+    	         System.out.println(a);
     	         
     	         continue;  
 
