852ccf4172bd1965488a3c96e4d7e2afbee2030e
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-11 16:12:08 +0900

    Update kadai_011.java

--- kadai_011.java
+++ kadai_011.java
@@ -1,47 +1,48 @@
 package text.section_11.java;
 
-	public class Arry_2 {
-	    public static void main(String[] args) {
+public class Arry_2 {
+	public static void main(String[] args) {
 
-	    int[] SosuuArray = {25}; //素数配列
-    	    int i = 0;             //i:判定される数字
+		int[] SosuuArray = new int[25]; //素数配列
+		int i = 0; //i:判定される数字
 
-    
-    	       for( int num = 1; num <= 101 ; num++ )   {  //num;2から始まる数字を100まで続ける、次からnum+1
-    	       if (num % 2 == 0)			//--------2割り切れない場合
-		 if (num == 2) {
-		 SosuuArry[i]=num;
-		　i++:}
+		for (int num = 1; num < 101; num++) { //num;2から始まる数字を100まで続ける、次からnum+1
+			if (num % 2 == 0) //--------2で割り切れる場合
+				if (num == 2) {//-------2の場合
+					SosuuArray[i] = num;//--素数2をSosuuArrayに収容
+					i++;
+				}
 
-	       if (num % 3 == 0)			//--------3割り切れない場合
-		 if (num == 3) {
-		 SosuuArry[i]=num;
-		 i++;}
-			 
-	       if (num % 5 == 0)			//--------5割り切れない場合
-		 if (num == 5) {
-		 SosuuArry[i]=num;
-		 i++; }
-			 
-	       if (num % 7 == 0)			//--------7割り切れない場合
-		 if (num == 7) {
-		 SosuuArry[i]=num;			 
-    	          i ++; }
+			if (num % 3 == 0) //--------3割り切れる場合
+				if (num == 3) {
+					SosuuArray[i] = num;
+					i++;
+				}
 
-	       if (num % 13 == 0)			//--------7割り切れない場合
-		 if (num == 13) {
-		 SosuuArry[i]=num;			 
-    	          i ++; }
-			 
-		else {
-		SosuuArry[i] = num;			//SosuuArry(i(0))に要素を１つ増やす
-			 
-		for (int a : Sosuu Array){ 
-    	         System.out.println(a);
-    	         
-    	         continue;  
+			if (num % 5 == 0) //--------5割り切れる場合
+				if (num == 5) {
+					SosuuArray[i] = num;
+					i++;
 
-    	       }
-	    }
+				}
 
+			if (num % 7 == 0) //--------7割り切れる場合
+				if (num == 7) {
+					SosuuArray[i] = num;
+					i++;
+
+				}
+
+				else {
+
+					for (int a : SosuuArray) {
+						System.out.println(a);
+
+						continue;
+					}
+				}
+
+		}
+
+	}
 }
