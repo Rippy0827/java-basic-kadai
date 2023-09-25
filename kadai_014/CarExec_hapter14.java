8083cc381a8e0eb19b11ce18e0445e0ceaa08e09
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-25 09:01:30 +0900

    Update CarExec_hapter14.java

--- kadai_014/CarExec_hapter14.java
+++ kadai_014/CarExec_hapter14.java
@@ -1,23 +1,17 @@
 package kadai_014;
 
-import kadai_014.Car_Chapter14.Product;
 
 public class CarExec_Chapter14 {
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	        //速度データを生成
-	        Product gear1 = new Product(gear[0], speed[0]);
-	        Product gear2 = new Product( 2);
-	        Product gear3 = new Product( 3, 3 );
-	        Product gear4 = new Product( 4, 40 );
-	        Product gear5 = new Product( 5, 50 );
+		//速度データを生成
+		Car_Chapter14 car = new Car_Chapter14();//実引数
 
-	        // 初期値gear1から変更後の変数gear3へ変更
-	        System.out.println("ギアを" + this.gear[num1] + "から" + this.gear[num2] + "に変更しました");
-	       	System.out.println("速度は時速" + this.speed[num2] + "kmです");
+		//Gearをチェンジする。
+		car.gearChange(3);
 
-
-	    }
-	
-
+		//速度を表示
+		car.run();
+	}
 }
+
