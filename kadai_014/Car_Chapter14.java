e8ac9a5b6fbe20dcdcdef9fa3581afe7784ed6a5
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-25 09:01:59 +0900

    Update Car_Chapter14.java

--- kadai_014/Car_Chapter14.java
+++ kadai_014/Car_Chapter14.java
@@ -1,35 +1,53 @@
 package kadai_014;
 
+//車の状態
 public class Car_Chapter14 {
+	//車のフィールド
+	private int[] speedRange = { 10, 20, 30, 40, 50 };//speedの可動域
+	int gear = 0;//gearの役割
+	int speed = 0;//speedの役割
 
-	//車の状態
-	public class Product {
-		//車のフィールド
-		private int[] gear = { 1, 2, 3, 4, 5 };
-		private int[] speed = { 10, 20, 30, 40, 50 };
-		//配列の呼び出し
-		int num1 = 0;
-		int num2 = 0;
+	//コンストラクタ(初期化処理)
+	public Car_Chapter14() {
+		//gearを初期化
+		this.gear = 1;
+		//speedを初期化
+		this.speed = 10;
+	}
 
-		//コンストラクタ(初期化処理)
-		public Product(int gear[], int speed[]) {
-			this.gear = gear;
-			this.speed = speed;
-		}
-
+	//ギアの値により速度を変える
+	public void gearChange(int afterGear) {
 		//ギアと速度の出力
-		public void SpeedOrder() {
-			System.out.println("ギアを" + this.gear[num1] + "から" + this.gear[num2] + "に変更しました");
-			System.out.println("速度は時速" + this.speed[num2] + "kmです");
-		}
-
-		//上記以外のギア
-		//num：gear配列を参照する変数。；・
-		public void GearNasi(int num) {
-			if (num > 6 || 1 > num) {
-				System.out.println("存在しないギアなので時速10kmです");
-				return ;
-			}
+		System.out.println("ギアを" + this.gear + "から" + afterGear + "に変更しました");
+		//やってきたgearのデータをnum1に代入(今回のギア)
+		this.gear = afterGear;
+		//速度の計算(Gearに沿った速度)
+		switch (gear) {
+		case 1:
+			this.speed = speedRange[0];
+			break;
+		case 2:
+			this.speed = speedRange[1];
+			break;
+		case 3:
+			this.speed = speedRange[2];
+			break;
+		case 4:
+			this.speed = speedRange[3];
+			break;
+		case 5:
+			this.speed = speedRange[4];
+			break;
+		default:
+			this.speed = 10;
+			break;
 		}
 	}
+
+	//ギアチェンジ後の速度を表示する
+	public void run() {
+		System.out.println("速度は時速" + speed + "kmです");
+	}
 }
+
+
