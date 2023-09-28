a73bbd928ab6d4fdcf2651eb84536609c23fa6f5
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-28 19:05:28 +0900

    Create KatoExec_Chapter17.java

---
+++ kadai_017/KatoExec_Chapter17.java
@@ -0,0 +1,21 @@
+package kadai_017;
+
+public class KatoExec_Chapter17 {
+
+	public static void main(String[] args) {
+		
+	 // Taroクラスの実行
+		Kato_Chapter17 Taro = new KatoTaro_Chapter17();
+		Taro.execIntroduce();
+        
+     //Ichiroクラスの実行
+		Kato_Chapter17 Ichiro = new KatoIchro_Chapter17();
+		Ichiro.execIntroduce();
+    
+   	 // Hanakoクラスの実行
+   		Kato_Chapter17 Hanako = new KatoHanako_Chapter17();
+   		Hanako.execIntroduce();
+	}
+
+	}
+
