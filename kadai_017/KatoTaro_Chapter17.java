4ee96b284e95dcd28f1ff172b23b09070fb74f85
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-09-28 19:03:50 +0900

    Create KatoTaro_Chapter17.java

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,19 @@
+package kadai_017;
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+	
+    @Override
+    protected String familyName() {
+        return "名前は加藤一郎です";
+    }
+
+    @Override
+    protected String address() {
+        return "住所は東京都中野区〇×です";
+    }
+    
+    @Override
+    protected String yourHobby() {
+        return "Javaが得意です";
+    }
+}
