75cd422ae9e3bf7c00fd9b794bf50e33d07e4fa2
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-10-03 18:19:54 +0900

    Update KatoHanako_Chapter17.java

--- kadai_017/KatoHanako_Chapter17.java
+++ kadai_017/KatoHanako_Chapter17.java
@@ -1,17 +1,7 @@
 package kadai_017;
 
 public class KatoHanako_Chapter17 extends Kato_Chapter17{
-
-    @Override
-    protected String familyName() {
-        return "名前は加藤+"setGivenName"+です";
-    }
-
-    @Override
-    protected String address() {
-        return "住所は東京都中野区〇×です";
-    }
-    
+   
     @Override
     protected String eachIntroduce() {
         return "趣味は読書です";
