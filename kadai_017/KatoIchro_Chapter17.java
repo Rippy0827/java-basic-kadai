1f41b1760679b227c85c87821695aa23ada519b0
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-10-03 11:14:55 +0900

    Update KatoIchro_Chapter17.java

--- kadai_017/KatoIchro_Chapter17.java
+++ kadai_017/KatoIchro_Chapter17.java
@@ -1,6 +1,6 @@
 package kadai_017;
 
-public class KatoIchro_Chapter17 extends Kato_Chapter17 {
+public class KatoIchiro_Chapter17 extends Kato_Chapter17 {
 	
     @Override
     protected String familyName() {
