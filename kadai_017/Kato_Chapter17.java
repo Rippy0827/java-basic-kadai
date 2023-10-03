fc0ed17cc7b4d7779cd52dd23a90ad97c0c27b0f
Rippy0827 <142187863+Rippy0827@users.noreply.github.com>
2023-10-03 18:19:19 +0900

    Update Kato_Chapter17.java

--- kadai_017/Kato_Chapter17.java
+++ kadai_017/Kato_Chapter17.java
@@ -1,21 +1,27 @@
 package kadai_017;
 
 public abstract class Kato_Chapter17 {
+	
+	public String familyName ="";
+	public String givenName = "";
+	public String address = "";
+	
+	public void setGivenName( String familyName, String givenName, String address ) {
+	       this.familyName	= familyName;
+	       this.givenName	= givenName;
+	       this.address 	= address;
+		}
+	
+	  public void execIntroduce() {
+			System.out.println("名前は"+ this.familyName + this.givenName + "です");	
+			System.out.println(address); 
+			System.out.println(eachIntroduce());
+			System.out.println("");
+		}
 
-	//実行メソッド
-	public void execIntroduce() {
-		System.out.println(familyName());
-		System.out.println(address());
-		System.out.println(eachIntroduce());
-	}
-
-	//抽象メソッド
-	abstract protected String familyName();
-
-	//抽象メソッド
-	abstract protected String address();
-
-	//抽象メソッド（）
-	abstract protected String eachIntroduce();
+	//抽象メソッド（好きなこと）
+	protected abstract String eachIntroduce();
+	
+	
 
 }
